68be34c2b5f018115dd3eef61e64823b3ff82d4e
Mubenassi <mubenassi@outlook.com>
2022-12-30 16:47:08 -0300

    Fixes

--- 05_firstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/atom/juice/GreetingController.java
+++ 05_firstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/atom/juice/GreetingController.java
@@ -3,7 +3,6 @@ package br.com.atom.juice;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
